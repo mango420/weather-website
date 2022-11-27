510b72d4d4c37a2c76ac2c23f2a5a8510a8c8ee3
mangoeis <60978002+CaptainMango1@users.noreply.github.com>
2022-11-27 20:56:06 +0100

    Data is now displayed in the index.html

--- js/client.js
+++ js/client.js
@@ -4,10 +4,23 @@ async function getData(city) {
     let cityId = cities[0].id;
     let current = await getCityCurrentInfo(cityId);
     document.getElementById('currTime').innerHTML = `&nbsp;${current.time}`;
+    //TODO change symbol if cond change
     document.getElementById('currCondition').innerHTML = `&nbsp;${current.symbolPhrase}`;
+    //TODO changes symbol if cold, warm etc.
     document.getElementById('currTemp').innerHTML = `&nbsp;${current.temperature} °C`;
     document.getElementById('currFeelTemp').innerHTML = `&nbsp;${current.feelsLikeTemp} °C`;
-    console.log(current);
+    document.getElementById('relHum').innerHTML = `&nbsp;${current.relHumidity} %`;
+    document.getElementById('dewPoint').innerHTML = `&nbsp;${current.dewPoint} °C`;
+    document.getElementById('windSpeed').innerHTML = `&nbsp;${current.windSpeed} m/s`;
+    document.getElementById('windDir').innerHTML = `&nbsp;${current.windDirString}`;
+    //TODO change symbol for cloudiness
+    document.getElementById('cloudiness').innerHTML = `&nbsp;${current.cloudiness}`;
+    //TODO change symbol for thunder prob
+    document.getElementById('thunderProb').innerHTML = `&nbsp;${current.thunderProb} %`;
+    //TODO change color
+    document.getElementById('uvIndex').innerHTML = `&nbsp;${current.uvIndex}`;
+    document.getElementById('pressure').innerHTML = `&nbsp;${current.pressure} hPa`;
+    document.getElementById('visibility').innerHTML = `&nbsp;${current.visibility}`;
 }
 async function getCityArray(city) {
     const options = {
--- js/client.ts
+++ js/client.ts
@@ -38,11 +38,24 @@ async function getData(city: string) {
     let current: any = await getCityCurrentInfo(cityId);
 
     document.getElementById('currTime')!.innerHTML = `&nbsp;${current.time}`;
+    //TODO change symbol if cond change
     document.getElementById('currCondition')!.innerHTML = `&nbsp;${current.symbolPhrase}`;
+    //TODO changes symbol if cold, warm etc.
     document.getElementById('currTemp')!.innerHTML = `&nbsp;${current.temperature} °C`;
     document.getElementById('currFeelTemp')!.innerHTML = `&nbsp;${current.feelsLikeTemp} °C`;
+    document.getElementById('relHum')!.innerHTML = `&nbsp;${current.relHumidity} %`;
+    document.getElementById('dewPoint')!.innerHTML = `&nbsp;${current.dewPoint} °C`;
+    document.getElementById('windSpeed')!.innerHTML = `&nbsp;${current.windSpeed} m/s`;
+    document.getElementById('windDir')!.innerHTML = `&nbsp;${current.windDirString}`;
+    //TODO change symbol for cloudiness
+    document.getElementById('cloudiness')!.innerHTML = `&nbsp;${current.cloudiness}`;
+    //TODO change symbol for thunder prob
+    document.getElementById('thunderProb')!.innerHTML = `&nbsp;${current.thunderProb} %`;
+    //TODO change color
+    document.getElementById('uvIndex')!.innerHTML = `&nbsp;${current.uvIndex}`;
 
-    console.log(current);
+    document.getElementById('pressure')!.innerHTML = `&nbsp;${current.pressure} hPa`;
+    document.getElementById('visibility')!.innerHTML = `&nbsp;${current.visibility}`;
 }
 
 async function getCityArray(city: string) {
